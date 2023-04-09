5969aa0f09c61e519a08846db2446f0a8ebf7528
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-04-09 22:48:39 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import { ContainerApp } from './App.styled';
 import { useAuth } from './hooks/useAuth';
-import { React, lazy } from 'react';
+import { React, lazy, useEffect } from 'react';
 import { GlobalStyle } from '../GlobalStyle';
+import { useDispatch } from 'react-redux';
 
 import { Route, Routes } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
 import { Layout } from './Layout';
+import { refreshUser } from './redux/auth/operations';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
@@ -14,8 +16,13 @@ const LoginPage = lazy(() => import('../pages/Login'));
 const ContactsPage = lazy(() => import('../pages/Contact'));
 
 export default function App() {
+    const dispatch = useDispatch();
    const { isRefreshing } = useAuth();
   
+  
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   return (
     !isRefreshing && (<ContainerApp>
